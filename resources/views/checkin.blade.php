9fc88e5a55adf88dcf499cbe8016254d1ce5a9b9
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-09-15 00:39:50 +0600

    Initial commit

---
+++ resources/views/checkin.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class='container bg-info-subtle bg-body-secondary'>
+        <div class='position-absolute top-50 start-50 translate-middle' style='width:50%'>
+            <div class=''>
+                <div class='rounded-4 shadow p-3 mb-5 bg-body-tertiary"'>
+                
+                        <div class='d-flex justify-content-center text-secondary m-3 fw-semibold fs-5'>ভিজিটরের তথ্য দিন</div>
+                        <form>
+                            <div class="mb-3 text-secondary">
+                                <label for="exampleInputEmail1" class="form-label">তারিখ:</label>
+                                <input type="email" class="form-control" id="" aria-describedby="emailHelp" disabled>
+                            </div>
+                            <div class="row justify-content-between">
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">নাম:</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1">
+                                </div>
+
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">মোবাইল নাম্বার:</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1">
+                                </div>
+                            </div>
+                            <div class="mb-3 text-secondary">
+                                <label for="exampleInputPassword1" class="form-label">পরিদর্শনের উদ্দেশ্য:</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="row justify-content-between">
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">অ্যাপার্টমেন্ট:</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1">
+                                </div>
+
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">ইউনিট নাম্বার:</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1">
+                                </div>
+                            </div>
+                            <div class="row justify-content-between">
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">চেক ইন:</label>
+                                    <input type="time" min="09:00" max="18:00" class="form-control" id="exampleInputPassword1">
+                                </div>
+
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">চেক আউট:</label>
+                                    <input type="time" min="09:00" max="18:00" class="form-control" id="exampleInputPassword1">
+                                </div>
+                            </div>
+                            <div class="row justify-content-between">
+                                <div class="mb-3 text-secondary col-6">
+                                    <label for="exampleInputPassword1" class="form-label">ভিজিটর নাম্বার:</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1">
+                                </div>
+                                <div class="mb-3 text-secondary col-6 d-flex align-items-end" >
+                                     <button type="submit" class="btn btn-primary w-20 " style="width: 20%;">Submit</button>
+                                </div>
+                            </div>
+                            
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
